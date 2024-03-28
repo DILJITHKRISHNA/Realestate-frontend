ef8df382769a69fd4712a6c6f2d8df5212efb37d
diljith krishna <diljithkrishna56@gmail.com>
2024-03-28 10:24:44 +0530

    Bugs cleared

--- src/Components/User/HomeBanner/HomeBanner.jsx
+++ src/Components/User/HomeBanner/HomeBanner.jsx
@@ -9,7 +9,7 @@ function HomeBanner() {
 
       <div className='flex justify-center items-center'>
         <h3 className='absolute font-extrabold text-white text-4xl sm:text-8xl w-[80%] font-mono text-center animate-pulse'>INTR<span className='text-black'>ODUCING</span > A N < span className='text-black' > EW</span> HOMELAND</h3>
-        <img className='2xl:mt-[20%]  object-contain max-w-full  max-h-full' src="/src/assets/images/BannerDemo.jpg" alt="" />
+        <img className='2xl:mt-[20%]  object-contain max-w-full  max-h-full' src="https://images.unsplash.com/photo-1624921938155-48a9a341d501?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         <ToastContainer />
       </div >
       <HomeCards />
