e338efe91ac7eb3f49d21c06f35e75e37006da8b
diljith krishna <diljithkrishna56@gmail.com>
2025-03-24 01:12:31 +0530

    banner page responsive issue solved

--- src/Components/User/HomeBanner/HomeBanner.jsx
+++ src/Components/User/HomeBanner/HomeBanner.jsx
@@ -4,24 +4,23 @@ import HomeCards from '../HomeCards/HomeCards';
 import Banner from '../../../assets/images/BannerDemo.jpg'
 
 function HomeBanner() {
-
   return (
     <>
-
-      <div className="flex flex-col justify-center items-center relative w-full min-h-screen overflow-hidden">
-        <h3 className="absolute font-extrabold text-white text-3xl sm:text-6xl lg:text-8xl w-[90%] max-w-7xl font-mono text-center animate-pulse leading-tight">
+      <div className="relative w-full h-screen">
+        <h3 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 font-extrabold text-white text-3xl sm:text-6xl lg:text-8xl w-[90%] max-w-7xl font-mono text-center animate-pulse leading-tight">
           INTR<span className="text-black">ODUCING</span> A N<span className="text-black">EW</span> HOMELAND
         </h3>
-        <img
-          className="sm:mt-24 lg:mt-14 w-full h-auto max-h-[90vh] object-cover"
-          src={Banner}
-          alt="Banner"
-        />
+        <div className="absolute inset-0">
+          <img
+            className="w-full h-full object-cover"
+            src={Banner}
+            alt="Banner"
+          />
+        </div>
         <ToastContainer />
       </div>
       <HomeCards />
     </>
-
   )
 }
 
